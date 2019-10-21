de18cd02df1169c64725305c2d7f08eeb4951ef7
Danielle Kang <daniellekang24@gmail.com>
2019-10-21 10:10:39 -0400

    not finished

--- src/area.js
+++ src/area.js
@@ -1,1 +1,11 @@
-sgdfg
+const readlineSync = require("readline-sync");
+
+const width = readlineSync.question("Width: ");
+const length = readlineSync.question("Length: ");
+const areaInches = (width * length);
+const areaMillimeters = areaInches * 25.4 * 25.4;
+
+
+console.log("A(n) " + width + "-by-" + length + "-inch sheet of paper has an area of " + areaMillimeters + " square millimeter(s).");
+
+//add: round the result to two decimals, format it with separating commas
