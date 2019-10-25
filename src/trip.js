66df0d17005a4d2cf0badd49caf25d17147c42b4
Danielle Kang <daniellekang24@gmail.com>
2019-10-25 09:53:33 -0400

    finished and working

--- src/trip.js
+++ src/trip.js
@@ -7,7 +7,7 @@ const busCapacity = parseInt(readlineSync.question("Bus capacity: "));
 const totalPeople = parseInt(studentNum + teacherNum);
 const busNum = Math.ceil(totalPeople/busCapacity);
 let passengersLastBus = parseFloat(totalPeople % busCapacity);
-passengersLastBus = passengersLastBus.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2 });
+passengersLastBus = passengersLastBus.toLocaleString("en", {minimumFractionDigits: 0, maximumFractionDigits: 0 });
 
 
-console.log(\nbusNum + " bus(es) is (are) needed, with " + passengersLastBus + " passenger(s) on the last bus.");
+console.log("\n" + busNum + " bus(es) is (are) needed, with " + passengersLastBus + " passenger(s) on the last bus.");
